ba8f713cef492149b4c1f43d2d200b6d7ff9e338
CleoMugs <cleophas.mugeni@gmail.com>
2020-10-08 10:15:31 +0300

    Initial Commit

--- scrap.py
+++ scrap.py
@@ -7,5 +7,11 @@ with open('simple.html') as html_file:
 
 #print(soup.prettify())
 
-match = soup.div
-print(match)
+for article in soup.find_all('div', class_='article'): 
+	headline = article.h2.a.text
+	print(headline)
+
+	summary = article.p.text
+	print(summary)
+
+	print()
